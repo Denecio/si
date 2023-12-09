deff93c0c5d59f332aaed5f1d46a5afd8d0f817d
Denecio <vascopereirabastos@gmail.com>
2023-12-09 18:39:08 +0000

    last purchases

--- scripts/profilelast.php
+++ scripts/profilelast.php
@@ -6,6 +6,10 @@
     $result = pg_fetch_all($result);
 
     //print the albums
+    $lastpurchases="SELECT name, artist, img FROM album WHERE name IN (SELECT album_name FROM itemoncart WHERE purchase_id IN (SELECT id FROM purchase WHERE client_utilizador_username='".$_SESSION["username"]."' and temp=false))";
+    $result = pg_query($connection, $lastpurchases);
+    $result = pg_fetch_all($result);
+
     for($i = (sizeof($result))-1; $i>(sizeof($result))-6; $i--){
         if($i>=0){
             echo "<a href='album.php?name=".$result[$i]['name']."' class='album_individual'>";
